a495ea0236ff548380c231968d08fe03741a4f1f
Lasata2790 <lasu.lasata@gmail.com>
2025-12-15 18:07:48 +0545

    Day 1: API project setup, added HelloController endpoint and tested!

---
+++ NGOVolunteerTaskManager/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NGOVolunteerTaskManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return "NGO Volunteer Task Manager - Day 1 started!";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ngo-volunteer-task-manager
 A web app to help NGOs manage volunteers, tasks, and events.
+
+
+
+
+
+# NGO Volunteer Task Manager
+
+A web app to help NGOs manage volunteers, tasks, and events.
+
+## Day 1 - 15/12/2025
+- Project setup using ASP.NET Core Web API
+- Added HelloController with test endpoint
+- Verified API runs successfully
